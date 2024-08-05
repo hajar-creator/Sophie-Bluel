4b7d1de16088a1e3ab5ea85b4946af3ebfac5f40
hajar-creator <hajaraitforaeso@gmail.com>
2024-08-05 19:18:37 +0200

    ajout modifications de la page d'index une fois l'utilisateur est en mode édition

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -47,3 +47,34 @@ async function submitForm() {
   });
 }
 submitForm();
+
+//Si utilisateur connecté
+function displayAdminMode() {
+  const token = sessionStorage.getItem("token");
+  if (token) {
+    //Ajouter bannière
+    const header = document.querySelector("header");
+    const addBanner = document.createElement("div");
+    addBanner.className = "banner";
+    addBanner.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
+        <span>Mode édition</span>`;
+    header.prepend(addBanner);
+
+    //Changer texte login à logout
+    const login = document.querySelector(".login__index");
+    login.innerHTML = `<a href="login.html" class="login__index"><li>logout</li></a>`;
+
+    //Enlever les boutons et ajouter icone édition
+    const boutons = document.querySelector(".btns");
+    console.log(boutons);
+    //divBtns.remove()
+
+    const modifier = document.createElement("div");
+    modifier.className = "modifier";
+    modifier.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
+        <span>modifier</span>`;
+    const portfolio = document.querySelector("#portfolio");
+    portfolio.prepend(modifier);
+  }
+}
+displayAdminMode();
