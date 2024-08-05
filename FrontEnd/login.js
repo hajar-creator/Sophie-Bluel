23bd3fc524a7ce5d6a1a6b067a376d3a16de15b2
hajar-creator <hajaraitforaeso@gmail.com>
2024-08-05 20:46:32 +0200

    améliorationde la bannière + suppréssion des boutons

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,80 +1,110 @@
-//Fonction pour envoyer requête à l'API pour se connecter au mode édition
-async function submitForm() {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", async function (event) {
-    event.preventDefault();
+//
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("test");
 
-    //Ciblage des champs email et password
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
+  //Fonction pour envoyer requête à l'API pour se connecter au mode édition
+  async function submitForm() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async function (event) {
+      event.preventDefault();
 
-    //Récupération de la valeur des champs email et password
-    const email = emailInput.value;
-    const password = passwordInput.value;
+      //Ciblage des champs email et password
+      const emailInput = document.getElementById("email");
+      const passwordInput = document.getElementById("password");
 
-    // Création d'un nouvel objet contenant les valeurs des champs du formulaire
-    const data = {
-      email: email,
-      password: password,
-    };
-    // Création de la charge utile au format JSON
-    const chargeUtile = JSON.stringify(data);
+      //Récupération de la valeur des champs email et password
+      const email = emailInput.value;
+      const password = passwordInput.value;
 
-    try {
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile,
-      });
+      // Création d'un nouvel objet contenant les valeurs des champs du formulaire
+      const data = {
+        email: email,
+        password: password,
+      };
+      // Création de la charge utile au format JSON
+      const chargeUtile = JSON.stringify(data);
 
-      //Récupération réponse API au format JSON
-      const result = await response.json();
-      console.log(result);
+      try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: chargeUtile,
+        });
 
-      //Validation avec token
-      if (response.ok && result.token) {
-        sessionStorage.setItem("token", result.token);
-        window.location.href = "index.html";
-      } else {
-        const erreurMessage = document.createElement("p");
-        erreurMessage.className = "erreur-login";
-        erreurMessage.innerText = "Identifiants incorrects";
-        document.querySelector("form").prepend(erreurMessage);
+        //Récupération réponse API au format JSON
+        const result = await response.json();
+        console.log(result);
+
+        //Validation avec token
+        if (response.ok && result.token) {
+          sessionStorage.setItem("token", result.token);
+          window.location.href = "index.html";
+        } else {
+          const erreurMessage = document.createElement("p");
+          erreurMessage.className = "erreur-login";
+          erreurMessage.innerText = "Identifiants incorrects";
+          document.querySelector("form").prepend(erreurMessage);
+        }
+      } catch (erreur) {
+        console.error(erreur);
       }
-    } catch (erreur) {
-      console.error(erreur);
-    }
-  });
-}
-submitForm();
-
-//Si utilisateur connecté
-function displayAdminMode() {
-  const token = sessionStorage.getItem("token");
-  if (token) {
-    //Ajouter bannière
-    const header = document.querySelector("header");
-    const addBanner = document.createElement("div");
-    addBanner.className = "banner";
-    addBanner.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
-        <span>Mode édition</span>`;
-    header.prepend(addBanner);
-
-    //Changer texte login à logout
-    const login = document.querySelector(".login__index");
-    login.innerHTML = `<a href="login.html" class="login__index"><li>logout</li></a>`;
-
-    //Enlever les boutons et ajouter icone édition
-    const boutons = document.querySelector(".btns");
-    console.log(boutons);
-    //divBtns.remove()
-
-    const modifier = document.createElement("div");
-    modifier.className = "modifier";
-    modifier.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
-        <span>modifier</span>`;
-    const portfolio = document.querySelector("#portfolio");
-    portfolio.prepend(modifier);
+    });
   }
-}
-displayAdminMode();
+  submitForm();
+
+  //Si utilisateur connecté
+  function displayAdminMode() {
+    let token = sessionStorage.getItem("token");
+    console.log(token);
+    if (token) {
+      //Ajouter bannière
+      const header = document.querySelector("header");
+      const addBanner = document.createElement("div");
+      addBanner.className = "banner";
+      addBanner.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
+        <span>Mode édition</span>`;
+      header.prepend(addBanner);
+
+      //Changer texte login à logout
+      const login = document.querySelector(".login__index");
+      login.innerHTML = `<a href="login.html" class="login__index"><li>logout</li></a>`;
+
+      //Enlever les boutons et ajouter icone édition
+      const boutons = document.querySelector(".boutons");
+      console.log(boutons);
+      boutons.remove();
+
+      const modifier = document.createElement("div");
+      modifier.className = "modifier";
+      modifier.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>
+        <span>modifier</span>`;
+      const portfolio = document.querySelector("#portfolio");
+      portfolio.prepend(modifier);
+    }
+  }
+  displayAdminMode();
+
+  //Modale
+
+  //Fonction pour ouvrir la modale
+  function openModal() {
+    const modifierIcone = document.querySelector(".modifier .fa-solid");
+    modifierIcone.addEventListener("click", function () {
+      console.log("cliqué");
+      const modal = document.querySelector(".modal");
+      modal.style.display = "block";
+    });
+  }
+  openModal();
+
+  //Fonction pour fermer la modale
+  function closeModal() {
+    const xmark = document.querySelector(".modal__content--icon");
+    xmark.addEventListener("click", function () {
+      console.log("cliqué");
+      const modal = document.querySelector(".modal");
+      modal.style.display = "none";
+    });
+  }
+  closeModal();
+});
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -34,11 +34,28 @@ genererWorks(works);
 // Fonction pour créations des boutons des filtres
 async function genererBtnCategories() {
   //Création des conteneurs pour les boutons et ajout du btn "Tous"
-  const divBtns = document.createElement("div");
-  divBtns.className = "btns";
+  // const divBtns = document.createElement("div");
+  // divBtns.className = "btns";
+  const divBtns = document.querySelector(".boutons");
   const buttonTous = document.createElement("button");
   buttonTous.className = "btn btn-selected";
   buttonTous.textContent = "Tous";
+
+  buttonTous.addEventListener("click", function () {
+    //On supprime la classe "btn-selected" de tous les boutons
+    const allBtns = divBtns.querySelectorAll("button");
+    for (let i = 0; i < allBtns.length; i++) {
+      allBtns[i].classList.remove("btn-selected");
+    }
+
+    //On ajoute la classe "btn-selected" que sur le bouton auquel s'applique l'eventListener
+    this.classList.add("btn-selected");
+
+    //filtrage des catégories
+    const categorie = this.textContent;
+    console.log(categorie);
+    filtrerCategories(categorie, works);
+  });
   //On rattache le btn "Tous" à la div qui l'aberge'
   divBtns.appendChild(buttonTous);
 
@@ -50,34 +67,54 @@ async function genererBtnCategories() {
   const gallerySection = document.getElementById("portfolio");
   // Boucle forEach pour la création et l'affichage du reste des boutons
   categories.forEach((categorie) => {
+    console.log(categorie);
     const categorieBtns = document.createElement("button");
     categorieBtns.className = "btn";
     categorieBtns.textContent = categorie.name;
-    //On rattache les boutons à la dic qui aberge les boutons puis à la section "gallery"
+    //On rattache les boutons à la div qui aberge les boutons puis à la section "gallery"
     divBtns.appendChild(categorieBtns);
     gallerySection.appendChild(divBtns);
     //On insère la div contenant les boutons avant les travaux
     const galleryDiv = document.querySelector("#portfolio .gallery");
     galleryDiv.parentNode.insertBefore(divBtns, galleryDiv);
 
+    console.log(divBtns);
     //On récupère tous les boutons de la div
     const allBtns = divBtns.querySelectorAll("button");
-    //On ajoute un écouteur d'évènements pour chaque bouton
-    allBtns.forEach((button) => {
-      button.addEventListener("click", function () {
-        //On supprime la classe "btn-selected" de tous les boutons
-        for (let i = 0; i < allBtns.length; i++) {
-          allBtns[i].classList.remove("btn-selected");
-        }
-        //On ajoute la classe "btn-selected" que sur le bouton auquel s'applique l'eventListener
-        button.classList.add("btn-selected");
 
-        //filtrage des catégories
-        const categorie = this.textContent;
-        console.log(categorie);
-        filtrerCategories(categorie, works);
-      });
+    categorieBtns.addEventListener("click", function () {
+      //On supprime la classe "btn-selected" de tous les boutons
+      const allBtns = divBtns.querySelectorAll("button");
+      for (let i = 0; i < allBtns.length; i++) {
+        allBtns[i].classList.remove("btn-selected");
+      }
+
+      //On ajoute la classe "btn-selected" que sur le bouton auquel s'applique l'eventListener
+      this.classList.add("btn-selected");
+
+      //filtrage des catégories
+      const categorie = this.textContent;
+      console.log(categorie);
+      filtrerCategories(categorie, works);
     });
+
+    //On ajoute un écouteur d'évènements pour chaque bouton
+    // allBtns.forEach((button) => {
+    //   console.log(button);
+    //   button.addEventListener("click", function () {
+    //     //On supprime la classe "btn-selected" de tous les boutons
+    //     for (let i = 0; i < allBtns.length; i++) {
+    //       allBtns[i].classList.remove("btn-selected");
+    //     }
+    //     //On ajoute la classe "btn-selected" que sur le bouton auquel s'applique l'eventListener
+    //     button.classList.add("btn-selected");
+
+    //     //filtrage des catégories
+    //     const categorie = this.textContent;
+    //     console.log(categorie);
+    //     filtrerCategories(categorie, works);
+    //   });
+    // });
   });
 }
 // On affiche les boutons en appelant la fonction
