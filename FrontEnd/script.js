d5eb3ae99ebb7035a2247bcc1ad6badbb4c77a19
hajar-creator <hajaraitforaeso@gmail.com>
2024-08-05 13:52:39 +0200

    Création d'une fonction pour afficher les travaux sur le site

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,3 +5,26 @@ async function getWorks() {
   return works;
 }
 const works = await getWorks();
+
+//Fonction pour récupérer les travaux de l'API et les afficher sur le site
+function genererWorks(works) {
+  works.forEach((work) => {
+    //Récupération de l'élément DOM qui accueillera les fiches
+    const sectionGallery = document.querySelector(".gallery");
+    // Création d'une balise dédiée à chaque oeuvre
+    const figureElement = document.createElement("figure");
+    // Création des balises img + leur contenu
+    const imageElement = document.createElement("img");
+    imageElement.src = work.imageUrl;
+    const captionElement = document.createElement("figcaption");
+    captionElement.innerText = work.title;
+
+    //On rattache les balises à la section gallery
+    sectionGallery.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(captionElement);
+  });
+}
+
+// On appelle la fonction genererWorks(works) pour afficher les travaux
+genererWorks(works);
